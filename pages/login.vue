45c5aa8c47cfedb90a365105b72cb99d5eb1174b
Tony-ArtZ <adarshgku@gmail.com>
2023-06-21 12:05:37 +0530

    added login page and integrated supabase module

---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pt-20 grid place-items-center h-full w-full">
+    <section class="bg-sky-950 flex flex-col items-center p-4 h-96  w-96 rounded-xl">
+      <h1 class="w-full text-white p-4 text-start font-bold text-4xl mb-8">Login</h1>
+      <label class="text-start text-green-400 w-full px-4">Email:</label>
+      <input placeholder="Email" type="email" class="rounded-full mb-8 font-bold text-white p-2 bg-slate-800 border-solid border-4 h-12 w-42 border-green-400"/>
+      <button class="p-6 w-44 text-lg bg-green-400 font-bold rounded-xl">Submit</button>
+    </section>
+  </div>
+</template>
