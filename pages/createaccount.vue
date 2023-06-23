d946c6d4ceac2de14e410207e17a2520728bf4c8
Tony-ArtZ <adarshgku@gmail.com>
2023-06-23 13:37:54 +0530

    added create account route and added upload functionality

---
+++ pages/createaccount.vue
@@ -0,0 +1,110 @@
+<script setup>
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+let imageBlob = ref();
+let previewImage = ref();
+let userName = ref();
+
+const imageChangeHandler = (e) => {
+  const file = e.target.files[0];
+  imageBlob.value = file;
+  previewImage.value = URL.createObjectURL(file);
+};
+
+const createAccount = async () => {
+  try {
+    if (!imageBlob.value || imageBlob.value.length === 0) {
+      throw new Error("You must select an image to upload.");
+    }
+    const imageExtention = imageBlob.value.type.split("/")[1]
+    const path = `/${user.id}/avatar.${imageExtention}`;
+    let { error: uploadError } = await supabase.storage
+      .from("users")
+      .upload(path, imageBlob.value);
+    if (uploadError) throw uploadError;
+  } catch (error) {
+    alert(error);
+  }
+};
+</script>
+
+<template>
+  <div class="pt-20 grid place-items-center h-screen w-full">
+    <section
+      class="
+        bg-sky-950
+        flex
+        border-green-400 border-4 border-solid
+        flex-col
+        items-center
+        p-4
+        w-full
+        rounded-xl
+      "
+    >
+      <h1 class="w-full text-white p-4 text-start font-bold text-4xl mb-8">
+        Create Account
+      </h1>
+      <!--Profile Picture-->
+      <input
+        required
+        id="profile-upload"
+        @change="imageChangeHandler"
+        type="file"
+        accept="image/jpg, image/png"
+        class="hidden"
+      />
+      <label
+        for="profile-upload"
+        class="
+          bg-slate-800
+          border-4
+          mb-4
+          border-green-400
+          text-white
+          grid
+          place-items-center
+          font-bold
+          h-32
+          w-32
+          rounded-full
+        "
+      >
+        <img
+          v-if="previewImage"
+          :src="previewImage"
+          class="rounded-full object-cover w-full aspect-square"
+        />
+        <div v-else>Upload Image</div>
+      </label>
+      <!--User Name-->
+      <label class="text-start mr-44 mb-2 font-bold text-green-400"
+        >User Name:</label
+      >
+      <input
+        v-model="userName"
+        placeholder="User Name"
+        type="text"
+        class="
+          rounded-full
+          mb-12
+          font-bold
+          text-white
+          p-4
+          bg-slate-800
+          border-solid border-4
+          h-12
+          w-42
+          border-green-400
+        "
+      />
+
+      <button
+        @click.prevent="createAccount"
+        class="p-6 w-44 text-lg bg-green-400 font-bold rounded-xl"
+      >
+        Submit
+      </button>
+    </section>
+  </div>
+</template>
