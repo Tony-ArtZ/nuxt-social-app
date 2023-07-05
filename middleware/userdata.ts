7df5b644a7ffb63efd163506a0eeb381d8fe813e
Tony-ArtZ <adarshgku@gmail.com>
2023-07-05 20:16:04 +0530

    added user check to userdata middleware

--- middleware/userdata.ts
+++ middleware/userdata.ts
@@ -1,9 +1,14 @@
-export default defineNuxtRouteMiddleware( async (to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = useSupabaseUser();
-const client = useSupabaseClient()
+  const client = useSupabaseClient();
 
-const {count, error} = await client.from("users").select('*', {count:"exact", head:true}).eq("id", user.value.id)
-  if (count <= 0 || !count) {
-    return navigateTo("/createaccount");
+  if (user.value) {
+    const { count, error } = await client
+      .from("users")
+      .select("*", { count: "exact", head: true })
+      .eq("id", user.value.id);
+    if (count <= 0 || !count) {
+      return navigateTo("/createaccount");
+    }
   }
 });
